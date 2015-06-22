883582042a138c9717f3694bee80978fbea9bc93
mrsantos <mrsantos.caxias@gmail.com>
2015-06-22 16:20:19 -0300

    ajustando esqueleto do projeto

---
+++ lib/cobrato/client/ruby.rb
@@ -0,0 +1,9 @@
+require "cobrato/client/ruby/version"
+
+module Cobrato
+  module Client
+    module Ruby
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cobrato/client/ruby/version.rb
@@ -0,0 +1,7 @@
+module Cobrato
+  module Client
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
