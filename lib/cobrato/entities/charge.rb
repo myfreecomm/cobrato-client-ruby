112b441f1c6e56d6eb5f6013e983063602c076c5
Hugo Maia Vieira <hugo.maia@myfreecomm.com.br>
2015-10-15 12:57:17 -0300

    Add new Charge attributes

--- lib/cobrato/entities/charge.rb
+++ lib/cobrato/entities/charge.rb
@@ -1,26 +1,35 @@
 module Cobrato
   module Entities
     class Charge < Base
-      attribute :id,                Integer
-      attribute :charge_account_id, Integer
-      attribute :due_date,          Date
-      attribute :processing_date,   Date
-      attribute :document_kind,     String
-      attribute :document_date,     Date
-      attribute :document_number,   String
-      attribute :custom_our_number, Boolean
-      attribute :our_number,        String
-      attribute :our_number_digit,  String
-      attribute :total_amount,      Decimal
-      attribute :instructions,      String
-      attribute :demonstrative,     String
-      attribute :payer_emails,      String
-      attribute :received,          Boolean
-      attribute :received_amount,   Decimal
-      attribute :received_at,       Date
-      attribute :processing_date,   Date
-      attribute :for_homologation,  Boolean
-      attribute :billet,            String
+      attribute :id,                  Integer
+      attribute :charge_account_id,   Integer
+      attribute :due_date,            Date
+      attribute :processing_date,     Date
+      attribute :document_kind,       String
+      attribute :document_date,       Date
+      attribute :document_number,     String
+      attribute :custom_our_number,   Boolean
+      attribute :our_number,          String
+      attribute :our_number_digit,    String
+      attribute :total_amount,        Decimal
+      attribute :instructions,        String
+      attribute :demonstrative,       String
+      attribute :payer_emails,        String
+      attribute :payer_info,          String
+      attribute :received,            Boolean
+      attribute :received_amount,     Decimal
+      attribute :received_at,         Date
+      attribute :processing_date,     Date
+      attribute :for_homologation,    Boolean
+      attribute :payment_method,      String
+      attribute :has_cnab_remittance, Boolean
+      attribute :billet_url,          String
+
+      def initialize(attrs)
+        attrs = JSON.parse(attrs.to_json, symbolize_names: true)
+        attrs[:billet_url] = attrs[:_links].find { |l| l[:rel] == 'billet' }[:href]
+        super attrs
+      end
     end
   end
 end
--- spec/cobrato/entities/charge_spec.rb
+++ spec/cobrato/entities/charge_spec.rb
@@ -16,16 +16,29 @@ describe Cobrato::Entities::Charge do
       instructions: "Pagável em qualquer agência até data do vencimento",
       demonstrative: "Demonstrativo",
       payer_emails: ["myemail@gmail.com"],
+      payer_info: "Empresa A - CNPJ X",
       received: true,
       received_amount: "10.07",
       received_at: "2015-01-30",
       processing_date: "2015-01-30",
       for_homologation: true,
-      billet: "https://cobrato-billet-storage.s3.amazonaws.com/"
+      payment_method: "billet",
+      has_cnab_remittance: false,
+      _links: [
+        { rel: "self",   method: "GET", href: "https://app.cobrato.com/api/v1/charges/1" },
+        { rel: "update", method: "PUT", href: "https://app.cobrato.com/api/v1/charges/1" },
+        { rel: "billet", method: "GET", href: "http://localhost:3000/charges/1/download_billet" }
+      ]
     }
   end
 
   subject { described_class.new(attributes) }
 
-  it_behaves_like "entity_attributes", [:id, :charge_account_id, :due_date, :document_kind, :document_date, :document_number, :custom_our_number, :our_number, :our_number_digit, :total_amount, :instructions, :demonstrative, :payer_emails, :received, :received_amount, :received_at, :processing_date, :for_homologation, :billet]
+  it_behaves_like "entity_attributes", [:id, :charge_account_id, :due_date, :document_kind, :document_date, :document_number, :custom_our_number, :our_number, :our_number_digit, :total_amount, :instructions, :demonstrative, :payer_emails, :received, :received_amount, :received_at, :processing_date, :for_homologation, :payer_info, :payment_method, :has_cnab_remittance, :billet_url]
+
+  context '#initialize' do
+    it "should set the corret billet_url based on links" do
+      expect(subject.billet_url).to eql("http://localhost:3000/charges/1/download_billet")
+    end
+  end
 end
