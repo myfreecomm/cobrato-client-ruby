ad84f92506f5d458bd6a467cdeb227cd58b30216
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2016-05-16 09:53:49 -0300

    Add support for ChargeConfig as PaymentGatewayConfig

--- lib/cobrato/entities/charge_config.rb
+++ lib/cobrato/entities/charge_config.rb
@@ -2,16 +2,18 @@ module Cobrato
   module Entities
     class ChargeConfig < Base
       attribute :id,                        Integer
+      attribute :name,                      String
       attribute :type,                      String
+      attribute :status,                    String
+
+      # billet specifics
       attribute :bank_account_id,           Integer
       attribute :portfolio_code,            String
       attribute :agreement_code,            String
       attribute :agreement_code_digit,      String
-      attribute :name,                      String
       attribute :initial_number,            Integer
       attribute :current_number,            Integer
       attribute :end_number,                Integer
-      attribute :status,                    String
       attribute :registered_charges,        Boolean
       attribute :agreement_number,          Integer
       attribute :initial_remittance_number, Integer
@@ -21,6 +23,12 @@ module Cobrato
       attribute :mulct_type,                String
       attribute :mulct_value,               Decimal
       attribute :transmission_code,         String
+
+      # payment gateway specifics
+      attribute :gateway_name,              String
+      attribute :gateway_id,                String
+      attribute :gateway_key,               String
+      attribute :use_avs,                   Boolean
     end
   end
 end
