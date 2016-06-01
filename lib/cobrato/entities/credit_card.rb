fb03699db7a23dfad7d094cd66d512788ca98f58
Hugo Maia Vieira <hugo.maia@nexaas.com>
2016-06-01 14:49:17 -0300

    Add CreditCard#charge_config_id

--- lib/cobrato/entities/credit_card.rb
+++ lib/cobrato/entities/credit_card.rb
@@ -1,18 +1,19 @@
 module Cobrato
   module Entities
     class CreditCard < Base
-      attribute :id,             Integer
-      attribute :number,         String
-      attribute :holder_name,    String
-      attribute :unusable,       Boolean
-      attribute :brand,          String
-      attribute :expiration,     String
-      attribute :avs_address,    String
-      attribute :avs_number,     String
-      attribute :avs_complement, String
-      attribute :avs_district,   String
-      attribute :avs_zipcode,    String
-      attribute :payer_id,       Integer
+      attribute :id,               Integer
+      attribute :number,           String
+      attribute :holder_name,      String
+      attribute :unusable,         Boolean
+      attribute :brand,            String
+      attribute :expiration,       String
+      attribute :avs_address,      String
+      attribute :avs_number,       String
+      attribute :avs_complement,   String
+      attribute :avs_district,     String
+      attribute :avs_zipcode,      String
+      attribute :payer_id,         Integer
+      attribute :charge_config_id, Integer
     end
   end
 end
---
+++ spec/cobrato/entities/credit_card_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe Cobrato::Entities::BankBillet do
+  let(:attributes) do
+    {
+      bank_code: '001',
+      bank_name: 'BancoDoBrasil',
+      portfolio_codes: ["11", "12", "16", "17", "18", "31", "51"],
+      regress: true,
+      remittance: false
+    }
+  end
+
+  subject { described_class.new(attributes) }
+
+  it_behaves_like "entity_attributes", [:bank_code, :bank_name, :portfolio_codes, :regress, :remittance]
+end
