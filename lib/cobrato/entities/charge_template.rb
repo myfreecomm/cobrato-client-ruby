7ae382d87bbb5507cb0d4e9e04cd00dbb5e328d5
João Mello <joao.mello@myfreecomm.com.br>
2016-11-24 11:18:13 -0200

    Remove parameters that won’t be used anymore

--- lib/cobrato/entities/charge_template.rb
+++ lib/cobrato/entities/charge_template.rb
@@ -3,11 +3,7 @@ module Cobrato
     class ChargeTemplate < Base
       attribute :id,                        Integer
       attribute :name,                      String
-      attribute :notification_emails,       String
       attribute :charged_amount,            Decimal
-      attribute :due_date,                  Date
-      attribute :document_date,             Date
-      attribute :document_number,           String
       attribute :document_kind,             String
       attribute :charge_config_id,          Integer
       attribute :instructions,              String
--- spec/cobrato/entities/charge_template_spec.rb
+++ spec/cobrato/entities/charge_template_spec.rb
@@ -6,11 +6,7 @@ describe Cobrato::Entities::ChargeTemplate do
     {
       id: "1",
       name: "Template novo",
-      notification_emails: "fulano@teste.com",
       charged_amount: 456.0,
-      due_date: "2016-11-30",
-      document_date: "2016-11-30",
-      document_number: "45ty67",
       document_kind: "DM",
       charge_config_id: "1",
       instructions: "",
@@ -25,5 +21,5 @@ describe Cobrato::Entities::ChargeTemplate do
 
   subject { described_class.new(attributes) }
 
-  it_behaves_like "entity_attributes", [:id, :name, :notification_emails, :charged_amount, :due_date, :document_date, :document_number, :document_kind, :charge_config_id, :instructions, :demonstrative, :registrable, :auto_send_billet, :interest_amount_per_month, :mulct_type, :mulct_value]
+  it_behaves_like "entity_attributes", [:id, :name, :charged_amount, :document_kind, :charge_config_id, :instructions, :demonstrative, :registrable, :auto_send_billet, :interest_amount_per_month, :mulct_type, :mulct_value]
 end
