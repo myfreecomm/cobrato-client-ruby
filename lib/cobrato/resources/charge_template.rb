ebe398e3cacb7468663822f4b919234bd16790c1
João Mello <joao.mello@myfreecomm.com.br>
2016-11-24 14:53:09 -0200

    Remove crud_request call at resource for charge_template

--- lib/cobrato/resources/charge_template.rb
+++ lib/cobrato/resources/charge_template.rb
@@ -1,46 +1,35 @@
 module Cobrato
   module Resources
     class ChargeTemplate < Base
-      crud :all
 
       def list(params={})
-        crud_request do
-          http.get("/charge_configs/#{params[:charge_config_id]}/charge_templates", params: params) do |response|
-            respond_with_collection(response)
-          end
+        http.get("/charge_configs/#{params[:charge_config_id]}/charge_templates", params: params) do |response|
+          respond_with_collection(response)
         end
       end
 
       def show(id, params)
-        crud_request do
-          http.get("/charge_configs/#{params[:charge_config_id]}/charge_templates/#{id}", params: params) do |response|
-            respond_with_entity(response)
-          end
+        http.get("/charge_configs/#{params[:charge_config_id]}/charge_templates/#{id}", params: params) do |response|
+          respond_with_entity(response)
         end
       end
 
       def update(id, params, parent)
-        crud_request do
-          http.put("/charge_configs/#{parent[:charge_config_id]}/charge_templates/#{id}", body: params) do |response|
-            respond_with_entity(response)
-          end
+        http.put("/charge_configs/#{parent[:charge_config_id]}/charge_templates/#{id}", body: params) do |response|
+          respond_with_entity(response)
         end
       end
 
       def destroy(id, parent)
-        crud_request do
-          http.delete("/charge_configs/#{parent[:charge_config_id]}/charge_templates/#{id}") do |response|
-            response.code == 204
-          end
+        http.delete("/charge_configs/#{parent[:charge_config_id]}/charge_templates/#{id}") do |response|
+          response.code == 204
         end
       end
 
 
       def create(params, parent)
-        crud_request do
-          http.post("/charge_configs/#{parent[:charge_config_id]}/charge_templates", body: params) do |response|
-            respond_with_entity(response)
-          end
+        http.post("/charge_configs/#{parent[:charge_config_id]}/charge_templates", body: params) do |response|
+          respond_with_entity(response)
         end
       end
     end
